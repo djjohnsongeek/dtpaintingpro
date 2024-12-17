75f14211b2dc6850e70d31d44110133e610a54ad
Daniel Johnson <danieleejohnson@gmail.com>
2024-12-17 05:03:12 -0500

    feat: add carosel images, remove google places api

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,7 @@
 class indexPage {
-    apiKey = "";
-    placeId = "ChIJFwNWkCuvU4gRPgBADymHygo";
-    textSearchUrl = "https://places.googleapis.com/v1/places:searchText";
-    placeDetailsUrl = `https://places.googleapis.com/v1/places/`;
-
     constructor()
     {
-        console.log("instantiation");
-
+        // Connect Form
         document.getElementById("contact-form-link").addEventListener("click", (event) => {
             const name = document.getElementById("connect-name").value;
             const email = document.getElementById("connect-email").value;
@@ -20,37 +14,10 @@ class indexPage {
             const subject = `I am Interested in DT Painting Pro - ${name}`;
             const body = `Name: ${name}\nAddress: ${address} ${address2}\nEmail: ${email}\nPhone: ${phone}\nDate: ${date}\nGoals: ${goals}`;
             const url = encodeURI(`mailto:dtpaintingpro@gmail.com?subject=${subject}&body=${body}`);
-
+            
             event.currentTarget.href = url;
         });
-    }
-
-    getPlaceId()
-    {
-        fetch(this.textSearchUrl, {
-            method: "POST",
-            body: JSON.stringify({
-                textQuery: "DT Painting Pro, Winston-Salem NC"
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-                "X-Goog-Api-Key": this.apiKey,
-                "X-Goog-FieldMask": "places.id,places.displayName"
-            }
-        })
-        .then((response) => response.json())
-        .then((json) => console.log(json));
-    }
-
-    getPlaceDetails(placeId)
-    {
-        let requestUrl = this.placeDetailsUrl + placeId + `?fields=id,displayName,photos&key=${this.apiKey}`;
-        fetch(requestUrl)
-            .then((response) => response.json())
-            .then((json) => console.log(json));
     }
 }
 
 var page = new indexPage();
-// page.getPlaceId();
-//page.getPlaceDetails(page.placeId);
